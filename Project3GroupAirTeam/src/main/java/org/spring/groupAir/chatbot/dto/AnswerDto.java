bdb06436dfc2237ad82bd592f2e3cc72f6b3573e
jomingeun <dgy6k@naver.com>
2024-06-20 14:46:36 +0900

    3차 프로젝트

---
+++ Project3GroupAirTeam/src/main/java/org/spring/groupAir/chatbot/dto/AnswerDto.java
@@ -0,0 +1,87 @@
+package org.spring.groupAir.chatbot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.spring.groupAir.movie.dto.DailyBoxOfficeList;
+import org.spring.groupAir.movie.dto.MovieInfo;
+import org.spring.groupAir.movie.dto.WeeklyBoxOfficeList;
+import org.spring.groupAir.bus.dto.bus.ItemList;
+import org.spring.groupAir.weather.dto.Main;
+import org.spring.groupAir.weather.dto.Weather;
+import org.spring.groupAir.weather.dto.WeatherDto;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class AnswerDto {
+
+    private Long id;
+
+    private String content;
+
+    private String keyword;
+
+    private EmployeeInfo employee;
+
+    private List<EmployeeInfo> employeeInfo;
+
+    private Main weather;
+
+    private List<Main> weatherTemp;
+
+
+    private WeeklyBoxOfficeList weeklyBoxOffice;
+
+    private List<WeeklyBoxOfficeList> weeklyBoxOfficeLists;
+
+    private MovieInfo movieInfo;
+
+    private List<MovieInfo> movieInfoList;
+
+    private ItemList bus;
+
+    private List<ItemList> busInfo;
+
+
+    private DailyBoxOfficeList dailyBoxOfficeList;
+
+    private List<DailyBoxOfficeList> dailyBoxOfficeListInfo;
+
+    public AnswerDto employeeInfo(EmployeeInfo employee) {
+        this.employee = employee;
+        return this;
+    }
+
+    public AnswerDto weatherTemp(Main weather) {
+        this.weather = weather;
+        return this;
+    }
+
+
+    public AnswerDto weeklyMovie(WeeklyBoxOfficeList weeklyBoxOffice) {
+        this.weeklyBoxOffice = weeklyBoxOffice;
+        return this;
+    }
+
+    public AnswerDto weeklyMovieDetail(MovieInfo movieInfo) {
+        this.movieInfo = movieInfo;
+        return this;
+    }
+
+
+    public AnswerDto busInfo(ItemList bus) {
+        this.bus = bus;
+        return this;
+    }
+    public AnswerDto dailyBoxOfficeList(DailyBoxOfficeList dailyBoxOfficeList) {
+        this.dailyBoxOfficeList = dailyBoxOfficeList;
+        return this;
+    }
+
+}
---
+++ Project3GroupAirTeam/src/main/java/org/spring/groupAir/movie/dto/DailyBoxOfficeList.java
@@ -0,0 +1,35 @@
+package org.spring.groupAir.movie.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DailyBoxOfficeList {
+
+  private String rank;
+  private String rnum;
+  private String rankInten;
+  private String rankOldAndNew;
+  private String movieCd;
+  private String movieNm;
+  private String openDt;
+  private String salesAmt;
+  private String salesShare;
+  private String salesInten;
+  private String salesChange;
+  private String salesAcc;
+  private String audiCnt;
+  private String audiInten;
+  private String audiChange;
+  private String audiAcc;
+  private String scrnCnt;
+  private String showCnt;
+
+
+}
